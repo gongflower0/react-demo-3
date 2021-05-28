6db38102f5227459dff188660eb5b3e20a496cc9
gongflower <gongjh0926@foxmail.com>
2021-05-28 14:47:41 +0800

    配置环境

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,2 +1,4 @@
-console.log('hi')
-const div = document.getElementById('div')
+import React from 'react';
+import ReactDom from 'react-dom';
+import Button from './button'
+ReactDom.render(<Button></Button>, document.body);
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
-    mode: 'production',
+    //mode: 'development',
     entry: {
         index: './lib/index.tsx'
     },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx']
+    },
     output: {
         path: path.resolve(__dirname, 'dist/lib'),
-        library: 'FUI',
+        library: 'REACT',
         libraryTarget: 'umd',
     },
     module: {
@@ -18,9 +21,23 @@ module.exports = {
             }
         ]
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'index.html'
-        })
-    ]
+    /*  plugins: [
+         new HtmlWebpackPlugin({
+             template: 'index.html'
+         })
+     ], */
+    /* externals: {
+        react: {
+            commonjs: 'react',
+            commonjs2: 'react',
+            amd: 'react',
+            root: 'React',
+        },
+        'react-dom': {
+            commonjs: 'react-dom',
+            commonjs2: 'react-dom',
+            amd: 'react-dom',
+            root: 'ReactDOM'
+        }
+    } */
 }
