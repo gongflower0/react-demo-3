8633b385c7cff7d25b565fda4c6e186c773a00cf
gongflower <gongjh0926@foxmail.com>
2021-05-30 15:58:02 +0800

    webpack配置全部完成

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,14 @@ module.exports = {
             {
                 test: /\.tsx?$/,
                 loader: 'awesome-typescript-loader'
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "less-loader",
+                ],
             }
         ]
     },
